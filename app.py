2d61e486deb87951e074060ad826f09838772499
SafetyZone <81029510+Muhammedska@users.noreply.github.com>
2022-02-21 17:20:33 +0300

    merka

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import sys
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
-from PyQt5.QtGui import QPixmap, QIcon
-from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtGui import *
+
 
 from PIL import Image
 from PIL.ImageQt import ImageQt
-
+import pyautogui
 
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
@@ -18,6 +18,22 @@ class Ui(QtWidgets.QMainWindow):
         self.setWindowFlags(Qt.FramelessWindowHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
 
+        #-- tools --
+        self.quit.clicked.connect(self.qq)
+        self.pen.clicked.connect(self.qq)
+        self.ss.clicked.connect(self.qq)
+        self.eraser.clicked.connect(self.qq)
+        
+        #--colors--
+        self.red.clicked.connect(self.qq)
+        self.blue.clicked.connect(self.qq)
+        self.green.clicked.connect(self.qq)
+        self.black.clicked.connect(self.qq)
+    def tss(self):
+        image = pyautogui.screenshot()
+    def qq(self):
+        self.destroy()
+        sys.exit()
 if __name__ == '__main__':
     # create pyqt5 app
     App = QApplication(sys.argv)
